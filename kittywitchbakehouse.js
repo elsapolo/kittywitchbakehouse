a7a21a650207d7c406692f0a442170ce3da4adf5
elsapolo <elsa.polo.laube@gmail.com>
2021-05-16 18:26:55 +0100

    validation of pizza types and timeslots

--- kittywitchbakehouse.js
+++ kittywitchbakehouse.js
@@ -1,5 +1,6 @@
 const PIZZAS_IN_SLOT = 6;
-
+const SLOTS = ["4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00"];
+const PIZZA_TYPES = ["MARGARITA", "VEGETARIAN", "ITALIAN"];
 
 // const PizzaType = {
 //     MARGARITA: "MARGARITA",
@@ -22,9 +23,15 @@ const PIZZAS_IN_SLOT = 6;
 var orders = [];
 
 function createOrder(name, pizzas, timeSlot){
+    if(!isTimeSlot(timeSlot)){
+        throw("oven isnt hot yet/anymore");
+    }
+    if(!arePizzas(pizzas)){
+        throw("no karen, choritso on pizza is not an option");
+    }
     if(!pizzaQuantityAvailable(pizzas, timeSlot)){
         //console.log("THIS SHOULD BE AN ERROR");
-        throw("why so many pizzaH")
+        throw("why so many pizzaH");
     }
     const order = {
         name,
@@ -36,13 +43,10 @@ function createOrder(name, pizzas, timeSlot){
 }
 
 function pizzaQuantityAvailable(pizzas, timeSlot) {
-    const slotPizzas = orders.reduce((accumulator , ord)=>{
-        if (ord.timeSlot === timeSlot){
-            return accumulator + ord.pizzas.length
-        }
-        return accumulator
-    }, 0);
-    console.log(slotPizzas);
+    const slotPizzas = orders
+        .filter(isInTimeSlot(timeSlot))
+        .map(getAmountOfPizzas)
+        .reduce(sum, 0);
     if ((PIZZAS_IN_SLOT - slotPizzas) < pizzas.length){
         return false;
     }
@@ -50,17 +54,71 @@ function pizzaQuantityAvailable(pizzas, timeSlot) {
     return true;
 }
 
-function getOrders(){
-    return orders
+//Timeslot and pizza type validation
+function isTimeSlot(timeSlot){
+    return SLOTS.includes(timeSlot)
 }
 
+function arePizzas(pizzas){
+    const validPizzaTypes = pizzas.map(getPizzaType).every(isPizzaType);
+    return validPizzaTypes
+}
 
-createOrder("Monty", ["pizza1", "pizza2", "pizza3"], "4:30");
+function isPizzaType(pizzaType){
+    return PIZZA_TYPES.includes(pizzaType)
+}
+
+function getPizzaType(pizza){
+    return pizza.type
+}
+
+//pizza quantity validation
+function isInTimeSlot(timeSlot){
+    return (order) => order.timeSlot === timeSlot;
+}
+
+function getAmountOfPizzas(order){
+    return order.pizzas.length
+}
+
+function sum(x , y){
+    return x+y
+}
+
+// function getAvailablePizzas(){
+    
+// }
+
+const validPizza = {
+    type: "MARGARITA"
+}
+const invalidPizza = {
+    type: "choriz0"
+}
+createOrder("Monty", [validPizza, validPizza, validPizza], "4:30");
 console.log(orders);
-createOrder("Elsa", ["pizza1", "pizza2"], "4:30");
+createOrder("Elsa", [validPizza, validPizza], "4:30");
 console.log(orders);
-createOrder("Oliver", ["pizza1", "pizza2"], "5:30");
+createOrder("Oliver", [validPizza, validPizza], "5:30");
 console.log(orders)
 
-createOrder("Elsa2", ["pizza1", "pizza2"], "4:30");
-console.log(orders)
+//too many pizzas in one slot
+try{
+    createOrder("Elsa2", [validPizza, validPizza], "4:30");
+}catch(error){
+    console.log(error)
+}
+
+//wrong pizza type
+try{
+    createOrder("Elsa2", [invalidPizza, "pizza2"], "6:30");
+}catch(error){
+    console.log(error)
+}
+
+//wrong pizza time slot
+try{
+    createOrder("Elsa2", [validPizza], "2:00");
+}catch(error){
+    console.log(error)
+}
